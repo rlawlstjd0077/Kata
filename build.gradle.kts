8ff2704275a9571e5293e07a8850473ccfd5be2b
rlawlstjd0077 <rlawlstjd077@gmail.com>
2022-06-14 08:47:46 +0900

    gradle setting

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+description = "Gradle Tutorial Basic"
+version = "1.0"
+
+group = "org.example"
+
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.register<Zip>("zip") {
+    from("src")
+}
+
+tasks.register("hello") {
+    doLast {
+        println("Hello World!")
+    }
+}

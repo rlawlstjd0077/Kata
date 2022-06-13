5132e02c830a7551410247869a094f95ff773fa0
rlawlstjd0077 <rlawlstjd077@gmail.com>
2022-06-14 08:47:47 +0900

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,25 @@
+object Versions {
+    // https://kotlinlang.org/docs/releases.html#release-details
+    const val kotlinVersion = "1.6.20"
+
+    // https://github.com/spring-projects/spring-boot/releases
+    const val springBootVersion = "2.6.6"
+
+    // https://plugins.gradle.org/plugin/io.spring.dependency-management
+    const val springDependencyManagementVersion = "1.0.11.RELEASE"
+    const val springCloudVersion = "2020.0.3"
+    const val springCloudAwsVersion = "2.2.6.RELEASE"
+    const val kassavaVersion = "2.1.0"
+    const val queryDslVersion = "4.4.0"
+
+    const val jibVersion = "3.1.4"
+
+    const val awsSdkVersion = "2.17.28"
+
+    const val slackApiVersion = "0.0.2"
+    const val emailApiVersion = "0.1.0"
+
+    const val stripeSdkVersion = "20.74.0"
+
+    const val kotlinxHtmlVersion = "0.7.3"
+}
